6c89bab33c06ed6495e0ba05866686a807b423a3
Mika Myrseth <mika@myrseth.org>
2022-10-05 12:25:01 +0200

    add prefix processing

--- CustomTypes/Prefixes.py
+++ CustomTypes/Prefixes.py
@@ -5,3 +5,25 @@ class Prefixes(Enum):
     CUSTOM = "CUSTOM"
     LOG = "LOG"
     DELTA = "DELTA"
+    LAGGED = "LAGGED"
+
+    def process_prefixes(name: str) -> tuple[list, str]:
+        prefixes_in_name = []
+        term_list = name.split("-")
+
+        # Find matching prefixes
+        for term in term_list:
+            if term in [e.value for e in Prefixes]:
+                prefixes_in_name.append(Prefixes[term])
+            if term.isdigit() and int(term) < 10:
+                prefixes_in_name.append(term)
+
+        # Remove matches from dataseries name
+        for prefix in prefixes_in_name:
+            if isinstance(prefix, Prefixes):
+                term_list.remove(prefix.value)
+            else:
+                term_list.remove(prefix)
+        dataseries_name = "-".join(term_list)
+
+        return prefixes_in_name, dataseries_name
