f9b1eaa049e8264630e3a4828de714f4ff8d664b
Mika Myrseth <mika@myrseth.org>
2023-01-20 08:40:56 +0100

    add new models

--- CustomTypes/Model.py
+++ CustomTypes/Model.py
@@ -10,7 +10,15 @@ import scipy
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
-
+from tensorflow import keras
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from pysr import PySRRegressor
 
 from pandas import DataFrame
 from CustomTypes.Dataseries import CustomDataseries, DataFrequency, Dataseries
@@ -93,6 +101,11 @@ class Model(object):
         # Remove lagged rows
         df = df.iloc[self.lags:, :]
 
+        print("mika var her")
+        print(list(self.weights.keys()))
+        model, prediction = symbolic_regression(df, list(self.weights.keys()), self.dependent_variable)
+        return model, prediction
+
         for index, row in df.iterrows():
             prediction = 0
             for series, weight in self.weights.items():
@@ -100,21 +113,9 @@ class Model(object):
 
             df.loc[index, 'OUTPUT'] = prediction
 
-        # AD hoc fasit
-        # dep_series = Dataseries.get_dataseries("NB-KKI")
-        # dep_df = dep_series.get_df(self.frequency, from_date, to_date)
-        # dep_df = Prefixes.process_df("DELTA", dep_df, "NOIWTOT Index")
-        # df["FASIT"] = dep_df
 
-        # print("PROCESSED MODEL!: ")
-        # print(df)
-        # print("Comparing")
-        # print(df[self.dependent_variable].tolist())
-        # print("to")
-        # print(df["OUTPUT"].tolist())
         r2 = r2_score(df[self.dependent_variable].tolist(),
                       df["OUTPUT"].tolist())
-        # print("Running model ", self.name, "with R2: ", r2, "from ", from_date, "to ", to_date)
 
         # calculate standard error of residuals
         residuals = df[self.dependent_variable] - df["OUTPUT"]
@@ -122,9 +123,6 @@ class Model(object):
 
         # extra -1 adjusts for alpha not being a parameter
         adjusted_r2 = 1 - (1-r2)*(len(df)-1)/(len(df)-len(self.weights)-1-1)
-        # pd.set_option('display.max_columns', None)
-        # pd.reset_option(“max_columns”)
-        # print(df.head())
 
         # remove key in dict
         self.weights.pop(self.dependent_variable, None)
@@ -192,6 +190,411 @@ class Model(object):
 
 
         return lm, df
+
+def create_windows(df, window_size=10):
+    X = []
+    for i in range(len(df) - window_size):
+        X.append(df[i:i + window_size])
+    X = np.array(X)
+    return X
+
+def symbolic_regression(df: pd.DataFrame, X_names: "list[str]", Y_name: str):
+    # df.drop("ALPHA", axis=1, inplace=True)
+
+    X = df[X_names]
+    Y = df[Y_name]
+
+    X.drop("ALPHA", axis=1, inplace=True)
+    X.drop(Y_name, axis=1, inplace=True)
+
+    #Remove dashes from column names
+    X.columns = [x.replace("-", "_") for x in X.columns]
+
+
+    print("names")
+    print(X_names)
+    print(Y_name)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.7, shuffle=False)
+
+    # Scale data
+    # scaler_x = StandardScaler()
+    # scaler_y = StandardScaler()
+    # X_train = scaler_x.fit_transform(X_train)
+    # Y_train = scaler_y.fit_transform(Y_train)
+
+    # Create model
+    model = PySRRegressor(
+    niterations=50000,  # < Increase me for better results
+    binary_operators=["+","-", "*", "/"],
+    unary_operators=[
+        "sqrt",
+        # "log",
+        "abs",
+        "cube",
+        # "pow"
+        "square",
+        # "cos",
+        "exp",
+        # "sin",
+        # "inv(x) = 1/x",
+        # ^ Custom operator (julia syntax)
+    ],
+    extra_sympy_mappings={"inv": lambda x: 1 / x},
+    # ^ Define operator for SymPy as well
+    loss="loss(x, y) = (x - y)^2",
+    # ^ Custom loss function (julia syntax)
+    model_selection = 'best'
+    )
+
+    model.fit(X_train, Y_train)
+
+    print(model)
+
+    # Make predictions
+    # X_test = scaler_x.transform(X_test)
+    # Y_test = scaler_y.transform(Y_test)
+    Y_pred_is = model.predict(X_train)
+    Y_pred_oos = model.predict(X_test)
+
+    # Evaluate model
+    print("Symbolic In sample")
+    print("R2: ", r2_score(Y_train, Y_pred_is))
+    print("MSE: ", mean_squared_error(Y_train, Y_pred_is))
+    print("MAE: ", mean_absolute_error(Y_train, Y_pred_is))
+
+
+    
+    print("Symbolic Out of Sample")
+    print("R2: ", r2_score(Y_test, Y_pred_oos))
+    print("MSE: ", mean_squared_error(Y_test, Y_pred_oos))
+    print("MAE: ", mean_absolute_error(Y_test, Y_pred_oos))
+
+    # Linear regression
+    lm = LinearRegression()
+    lm.fit(X_train, Y_train)
+    Y_pred_is_lm = lm.predict(X_train)
+    print("Linear Regression In sample")
+    print("R2: ", r2_score(Y_train, Y_pred_is_lm))
+    print("MSE: ", mean_squared_error(Y_train, Y_pred_is_lm))
+    print("MAE: ", mean_absolute_error(Y_train, Y_pred_is_lm))
+
+    Y_pred_oos_lm = lm.predict(X_test)
+    print("Linear Regression Out of Sample")
+    print("R2: ", r2_score(Y_test, Y_pred_oos_lm))
+    print("MSE: ", mean_squared_error(Y_test, Y_pred_oos_lm))
+    print("MAE: ", mean_absolute_error(Y_test, Y_pred_oos_lm))
+
+    return model, Y_pred_oos_lm
+
+
+def random_forrest_regression(df: pd.DataFrame, X_names: "list[str]", Y_name: str):
+    # df.drop("ALPHA", axis=1, inplace=True)
+
+    X = df[X_names]
+    Y = df[Y_name]
+
+    X.drop("ALPHA", axis=1, inplace=True)
+    X.drop(Y_name, axis=1, inplace=True)
+
+    print("names")
+    print(X_names)
+    print(Y_name)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.7, shuffle=False)
+
+    # Scale data
+    # scaler_x = StandardScaler()
+    # scaler_y = StandardScaler()
+    # X_train = scaler_x.fit_transform(X_train)
+    # Y_train = scaler_y.fit_transform(Y_train)
+
+    # Create model
+    model = RandomForestRegressor(n_estimators=100, n_jobs=-1, verbose=1, max_depth=5)
+    model.fit(X_train, Y_train)
+
+    # Make predictions
+    # X_test = scaler_x.transform(X_test)
+    # Y_test = scaler_y.transform(Y_test)
+    Y_pred_is = model.predict(X_train)
+    Y_pred_oos = model.predict(X_test)
+
+    # Evaluate model
+    print("Forrest In sample")
+    print("R2: ", r2_score(Y_train, Y_pred_is))
+    print("MSE: ", mean_squared_error(Y_train, Y_pred_is))
+    print("MAE: ", mean_absolute_error(Y_train, Y_pred_is))
+
+
+    
+    print("Forrest Out of Sample")
+    print("R2: ", r2_score(Y_test, Y_pred_oos))
+    print("MSE: ", mean_squared_error(Y_test, Y_pred_oos))
+    print("MAE: ", mean_absolute_error(Y_test, Y_pred_oos))
+
+    # Linear regression
+    lm = LinearRegression()
+    lm.fit(X_train, Y_train)
+    Y_pred_is_lm = lm.predict(X_train)
+    print("Linear Regression In sample")
+    print("R2: ", r2_score(Y_train, Y_pred_is_lm))
+    print("MSE: ", mean_squared_error(Y_train, Y_pred_is_lm))
+    print("MAE: ", mean_absolute_error(Y_train, Y_pred_is_lm))
+
+    Y_pred_oos_lm = lm.predict(X_test)
+    print("Linear Regression Out of Sample")
+    print("R2: ", r2_score(Y_test, Y_pred_oos_lm))
+    print("MSE: ", mean_squared_error(Y_test, Y_pred_oos_lm))
+    print("MAE: ", mean_absolute_error(Y_test, Y_pred_oos_lm))
+
+    return model, Y_pred_oos_lm
+
+
+def xgboost_regression(df: pd.DataFrame, X_names: "list[str]", Y_name: str):
+    # df.drop("ALPHA", axis=1, inplace=True)
+
+    
+    X = df[X_names]
+    Y = df[Y_name]
+
+    X.drop("ALPHA", axis=1, inplace=True)
+    X.drop(Y_name, axis=1, inplace=True)
+
+    print("names")
+    print(X_names)
+    print(Y_name)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.7, shuffle=False)
+
+    # Scale data
+    scaler_x = StandardScaler()
+    scaler_y = StandardScaler()
+    X_train = scaler_x.fit_transform(X_train)
+    X_test = scaler_x.transform(X_test)
+    Y_train = scaler_y.fit_transform(Y_train.values.reshape(-1, 1))
+    Y_test = scaler_y.transform(Y_test.values.reshape(-1, 1))
+
+    # Create model
+    model = XGBRegressor()
+    model.fit(X_train, Y_train)
+
+    # Evaluate model
+    Y_pred_is = model.predict(X_train)
+    print("XGBoost In sample")
+    print("R2: ", r2_score(Y_train, Y_pred_is))
+    print("MSE: ", mean_squared_error(Y_train, Y_pred_is))
+    print("MAE: ", mean_absolute_error(Y_train, Y_pred_is))
+
+
+    Y_pred_oos = model.predict(X_test)
+    print("XGBoost OOS")
+    print("R2: ", r2_score(Y_test, Y_pred_oos))
+    print("MSE: ", mean_squared_error(Y_test, Y_pred_oos))
+    print("MAE: ", mean_absolute_error(Y_test, Y_pred_oos))
+
+    # Linear regression
+    lm = LinearRegression()
+    lm.fit(X_train, Y_train)
+    Y_pred_is_lm = lm.predict(X_train)
+    print("Linear Regression In sample")
+    print("R2: ", r2_score(Y_train, Y_pred_is_lm))
+    print("MSE: ", mean_squared_error(Y_train, Y_pred_is_lm))
+    print("MAE: ", mean_absolute_error(Y_train, Y_pred_is_lm))
+
+    Y_pred_oos_lm = lm.predict(X_test)
+    print("Linear Regression OOS")
+    print("R2: ", r2_score(Y_test, Y_pred_oos_lm))
+    print("MSE: ", mean_squared_error(Y_test, Y_pred_oos_lm))
+    print("MAE: ", mean_absolute_error(Y_test, Y_pred_oos_lm))
+
+    return model, Y_pred_oos
+
+
+def lstm_regression2(df: pd.DataFrame, X_names: "list[str]", Y_name: str):
+    # df.drop("ALPHA", axis=1, inplace=True)
+
+    
+    X = df[X_names]
+    Y = df[Y_name]
+
+    X.drop("ALPHA", axis=1, inplace=True)
+    X.drop(Y_name, axis=1, inplace=True)
+
+    print("names")
+    print(X_names)
+    print(Y_name)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.7, shuffle=False)
+
+    # Scale data
+    scaler_x = StandardScaler()
+    scaler_y = StandardScaler()
+    X_train = scaler_x.fit_transform(X_train)
+    X_test = scaler_x.transform(X_test)
+    Y_train = scaler_y.fit_transform(Y_train.values.reshape(-1, 1))
+    Y_test = scaler_y.transform(Y_test.values.reshape(-1, 1))
+
+    # Create windows
+    window_size = 52
+    X_train_window = create_windows(X_train, window_size)
+    Y_train_window = Y_train[window_size:]
+    X_test_window = create_windows(X_test, window_size)
+    Y_test_window = Y_test[window_size:]
+
+    X_train_lin = X_train[window_size:]
+    Y_train_lin = Y_train[window_size:]
+    X_test_lin = X_test[window_size:]
+    Y_test_lin = Y_test[window_size:]
+
+    print("X_train")
+    print(X_train)
+    print("Y_train")
+    print(Y_train)
+
+    model = keras.Sequential([
+            # keras.layers.Input(batch_shape=(None, None, 1)),
+            # keras.layers.LSTM(5, return_sequences=True),
+            keras.layers.LSTM(50, input_shape=(window_size, 1)),
+            # keras.layers.LSTM(10),
+            # keras.layers.Dense(8),
+            keras.layers.Dense(1),
+        ])
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
+    
+    # train_data = keras.preprocessing.sequence.TimeseriesGenerator(X_train, Y_train, length=1, batch_size=1)
+    histroy = model.fit(X_train_window, Y_train_window, epochs=20, verbose=1, validation_data=(X_test_window, Y_test_window))
+
+    # Predict on test data
+    predictions = model.predict(X_train_window)
+    mse_lstm = mean_squared_error(Y_train_window, predictions)
+    r2_lstm = r2_score(Y_train_window, predictions)
+    oos_predictions = model.predict(X_test_window)
+    oos_mse_lstm = mean_squared_error(Y_test_window, oos_predictions)
+    oos_r2_lstm = r2_score(Y_test_window, oos_predictions)
+
+    # compare with linear regression
+    from sklearn.linear_model import LinearRegression
+    lr_model = LinearRegression()
+    #reshape X
+    # X_train = np.array(X_train).reshape(-1, 1)
+    # print(X_train)
+    # print(Y_train)
+    lr_model.fit(X_train_lin, Y_train_lin)
+    predictions = lr_model.predict(X_train_lin)
+    mse_lin = mean_squared_error(Y_train_lin, predictions)
+    r2_lin = r2_score(Y_train_lin, predictions)
+    oos_predictions = lr_model.predict(X_test_lin)
+    oos_mse_lin = mean_squared_error(Y_test_lin, oos_predictions)
+    oos_r2_lin = r2_score(Y_test_lin, oos_predictions)
+
+    # Base mse
+    base = np.mean(Y_train)
+    base = np.repeat(base, len(Y_train))
+    mse_base = mean_squared_error(Y_train, base)
+    base = np.mean(Y_test)
+    base = np.repeat(base, len(Y_test))
+    oos_mse_base = mean_squared_error(Y_test, base)
+
+    print('Base MSE: ', mse_base)
+    print('Linear MSE: ', mse_lin)
+    print('LSTM MSE: ', mse_lstm)
+    print("")
+    print('Base OOS MSE: ', oos_mse_base)
+    print('Linear OOS MSE: ', oos_mse_lin)
+    print('LSTM OOS MSE: ', oos_mse_lstm)
+    print("")
+    print('Linear R2: ', r2_lin)
+    print('LSTM R2: ', r2_lstm)
+    print("")
+    print('Linear OOS R2: ', oos_r2_lin)
+    print('LSTM OOS R2: ', oos_r2_lstm)
+
+    return
+
+def lstm_regression(df: pd.DataFrame, X_names: "list[str]", Y_name: str):
+    # df.drop("ALPHA", axis=1, inplace=True)
+
+    X = df[X_names]
+    Y = df[Y_name]
+
+    X.drop("ALPHA", axis=1, inplace=True)
+    X.drop(Y_name, axis=1, inplace=True)
+
+    print("names")
+    print(X_names)
+    print(Y_name)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.5, random_state=101)
+
+    # Scale data
+    scaler_x = StandardScaler()
+    scaler_y = StandardScaler()
+    X_train = scaler_x.fit_transform(X_train)
+    X_test = scaler_x.transform(X_test)
+    Y_train = scaler_y.fit_transform(Y_train.values.reshape(-1, 1))
+    Y_test = scaler_y.transform(Y_test.values.reshape(-1, 1))
+
+    print("X_train")
+    print(X_train)
+    print("Y_train")
+    print(Y_train)
+
+    model = keras.Sequential([
+            # keras.layers.Input(batch_shape=(None, None, 1)),
+            # keras.layers.LSTM(5, return_sequences=True),
+            keras.layers.Dense(8, input_dim=4),
+            keras.layers.Dense(4),
+            keras.layers.Dense(2),
+            keras.layers.Dense(1),
+        ])
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
+    
+    # train_data = keras.preprocessing.sequence.TimeseriesGenerator(X_train, Y_train, length=1, batch_size=1)
+    histroy = model.fit(X_train, Y_train, epochs=10000)
+
+    # Predict on test data
+    predictions = model.predict(X_train)
+    mse_lstm = mean_squared_error(Y_train, predictions)
+    oos_predictions = model.predict(X_test)
+    oos_mse_lstm = mean_squared_error(Y_test, oos_predictions)
+
+    # compare with linear regression
+    from sklearn.linear_model import LinearRegression
+    lr_model = LinearRegression()
+    #reshape X
+    # X_train = np.array(X_train).reshape(-1, 1)
+    # print(X_train)
+    # print(Y_train)
+    lr_model.fit(X_train, Y_train)
+    predictions = lr_model.predict(X_train)
+    mse_lin = mean_squared_error(Y_train, predictions)
+    oos_predictions = lr_model.predict(X_test)
+    oos_mse_lin = mean_squared_error(Y_test, oos_predictions)
+
+    # Base mse
+    base = np.mean(Y_train)
+    base = np.repeat(base, len(Y_train))
+    mse_base = mean_squared_error(Y_train, base)
+    base = np.mean(Y_test)
+    base = np.repeat(base, len(Y_test))
+    oos_mse_base = mean_squared_error(Y_test, base)
+
+    print('Base MSE: ', mse_base)
+    print('Linear MSE: ', mse_lin)
+    print('LSTM MSE: ', mse_lstm)
+    print("")
+    print('Base OOS MSE: ', oos_mse_base)
+    print('Linear OOS MSE: ', oos_mse_lin)
+    print('LSTM OOS MSE: ', oos_mse_lstm)
+
+    return
+
+    return model, model
 
 
 def regression(df: pd.DataFrame, X_names: "list[str]", Y_name: str) -> LinearRegression:
---
+++ masters.py
@@ -0,0 +1,99 @@
+import json
+import os
+from CustomTypes.Dataseries import CustomDataseries, Dataseries
+from CustomTypes.Model import Model
+
+
+def main():
+    all_models = load_data()
+
+    # Get model where name is "Benchmark ICE-BRENT short"
+    model = next(
+        model for model in all_models if model.name == "Myrstuen&Sylte2020 short")
+    model.run_model(
+                    model.model_start_date, model.model_end_date)
+
+def load_data():
+    # Load data
+    all_dataseries = []
+    directory = "input/dataseries"
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print("Loading", f)
+            dataseries_json = open(f)
+            dataseries = json.load(dataseries_json)
+            dataseries = list(map(lambda json: Dataseries(**json), dataseries))
+            dataseries_json.close()
+            all_dataseries = all_dataseries+dataseries
+    Dataseries.data = all_dataseries
+
+    # Data debugging
+    if True:
+        for dataseries in all_dataseries:
+            if dataseries.df.empty:
+                print(f"{dataseries.name}: MISSING")
+            else:
+                print(
+                    f"{dataseries.name}:{dataseries.bbg_ticker} has data from {dataseries.df.index[0]} to {dataseries.df.index[-1]}")
+
+    all_custom_dataseries = []
+    directory = "input/custom_dataseries"
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print("Loading", f)
+            custom_dataseries_json = open(f)
+            custom_dataseries = json.load(custom_dataseries_json)
+            custom_dataseries = list(
+                map(lambda json: CustomDataseries(**json), custom_dataseries))
+            custom_dataseries_json.close()
+            all_custom_dataseries = all_custom_dataseries + custom_dataseries
+    CustomDataseries.data = all_custom_dataseries
+
+    long_model_dirs = [
+        "input/models_long",
+        "input/models_benchmarks_long",
+    ]
+
+    short_model_dirs = [
+        "input/models_short",
+        "input/models_benchmarks_short",
+    ]
+
+    all_long_models: list[Model] = []
+    all_short_models: list[Model] = []
+
+    for model_dir in long_model_dirs:
+        directory = model_dir
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                print("Loading", f)
+                model_json = open(f)
+                model = json.load(model_json)
+                model = Model(**model)
+                model_json.close()
+                all_long_models.append(model)
+
+    for model_dir in short_model_dirs:
+        directory = model_dir
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                print("Loading", f)
+                model_json = open(f)
+                model = json.load(model_json)
+                model = Model(**model)
+                model_json.close()
+                all_short_models.append(model)
+
+    all_models = all_long_models + all_short_models
+    return all_models
+
+
+main()
